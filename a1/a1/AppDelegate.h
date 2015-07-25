99c83e179e6eb7d9d6c03cc2dfab3c07d42665b5
hy <hy@hytekiMac-mini.local>
2015-07-25 15:32:59 +0800

    OK

---
+++ a1/a1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  a1
+//
+//  Created by hy on 15/7/25.
+//  Copyright (c) 2015年 com.yuanchuanglian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ a1/a1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  a1
+//
+//  Created by hy on 15/7/25.
+//  Copyright (c) 2015年 com.yuanchuanglian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
